8ad40b71d0fa121a6cea0fdf4b41a30c06c62b81
muhammad.fazal <fazal.abbas4@gmail.com>
2020-10-19 17:00:41 +0500

    Add Activity Section 06.

---
+++ app/src/main/java/edu/aku/hassannaqvi/spsa_afg/ui/sections/Section06Activity.java
@@ -0,0 +1,115 @@
+package edu.aku.hassannaqvi.spsa_afg.ui.sections;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Clear;
+import com.validatorcrawler.aliazaz.Validator;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.aku.hassannaqvi.spsa_afg.R;
+import edu.aku.hassannaqvi.spsa_afg.contracts.FormsContract;
+import edu.aku.hassannaqvi.spsa_afg.core.DatabaseHelper;
+import edu.aku.hassannaqvi.spsa_afg.core.MainApp;
+import edu.aku.hassannaqvi.spsa_afg.databinding.ActivitySection06Binding;
+
+import edu.aku.hassannaqvi.spsa_afg.databinding.ActivitySection09Binding;
+import edu.aku.hassannaqvi.spsa_afg.ui.other.EndingActivity;
+import edu.aku.hassannaqvi.spsa_afg.utils.AppUtilsKt;
+
+import static edu.aku.hassannaqvi.spsa_afg.CONSTANTS.FSTATUS_END_FLAG;
+import static edu.aku.hassannaqvi.spsa_afg.core.MainApp.form;
+import static edu.aku.hassannaqvi.spsa_afg.utils.AppUtilsKt.contextBackActivity;
+
+
+public class Section06Activity extends AppCompatActivity {
+
+    ActivitySection06Binding bi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section06);
+        bi.setCallback(this);
+
+        setupSkips();
+    }
+
+    private void setupSkips() {
+
+
+
+    }
+
+
+
+    private void SaveDraft() throws JSONException {
+
+        JSONObject json = new JSONObject();
+        json.put("s6q1a", bi.s6q1a.getText().toString());
+
+        json.put("s6q1b", bi.s6q1b.getText().toString());
+
+        json.put("s6q2a", bi.s6q2a.getText().toString());
+
+        json.put("s6q2b", bi.s6q2b.getText().toString());
+
+        json.put("s6q3", bi.s6q3.getText().toString());
+
+
+
+
+    }
+    public void onBackPressed() {
+        contextBackActivity(this);
+    }
+
+    public void BtnContinue() {
+        if (!formValidation()) return;
+        try {
+            SaveDraft();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (UpdateDB()) {
+            finish();
+            finish();
+            startActivity(new Intent(this, EndingActivity.class).putExtra("complete", true));
+        } else {
+            Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void BtnEnd() {
+        AppUtilsKt.openFormEndActivity(this, FSTATUS_END_FLAG, 2);
+    }
+
+
+    private boolean UpdateDB() {
+//        DatabaseHelper db = MainApp.appInfo.getDbHelper();
+//        int updcount = db.updatesFormColumn(FormsContract.FormsTable., form.sKtoString());
+//        if (updcount > 0) {
+//            return true;
+//        } else {
+//            Toast.makeText(this, "Sorry. You can't go further.\n Please contact IT Team (Failed to update DB)", Toast.LENGTH_SHORT).show();
+//            return false;
+//        }
+        return true;
+    }
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+
+    }
+
+
+
+}
